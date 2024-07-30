2990941c21560910aad0d431722ae844a905371f
부수환 <rndjdieo119@naver.com>
2024-07-30 22:23:15 +0900

    init : env.d.ts 셋팅

--- Tripeer/src/App.tsx
+++ Tripeer/src/App.tsx
@@ -1,7 +1,8 @@
 function App() {
+  console.log(import.meta.env.VITE_API_URL);
   return (
     <>
-      <div style={{ fontWeight: "bold", fontSize: "50px" }}>
+      <div style={{ fontWeight: 'bold', fontSize: '50px' }}>
         함께 여행을, 함께 계획하다.
       </div>
     </>
