613a9402c06b6e19c5d2d8df889feb4a650f7a50
부수환 <rndjdieo119@naver.com>
2024-08-06 21:39:40 +0900

    feat : 햄버거 버튼 반응형 추가 진행중

--- Tripeer/src/components/nav/NavBar.tsx
+++ Tripeer/src/components/nav/NavBar.tsx
@@ -1,5 +1,5 @@
 // 내부 모듈
-import FullLogo from '../FullLogo';
+import FullLogo from './FullLogo';
 import AuthButton from './AuthButton';
 import styles from './navbar.module.css';
 import NavButton from './NavButton';
@@ -8,7 +8,10 @@ export default function NavBar() {
   return (
     <nav className={styles.container}>
       <FullLogo />
+      <input type="checkbox" id="hamburger" />
+      <label htmlFor="hamburger" className={styles.hamburger} />
       <ul className={styles.linkBox}>
+        <hr className={styles.AuthLine} />
         <NavButton to="/plan" text="일정 계획" />
         <NavButton to="/diary" text="지난 여행" />
         <NavButton to="/place" text="여행지" />
