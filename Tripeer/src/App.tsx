2ec084b6d4bb587482796224a871619ff514180d
부수환 <rndjdieo119@naver.com>
2024-08-06 22:44:35 +0900

    feat : login 페이지 추가

--- Tripeer/src/App.tsx
+++ Tripeer/src/App.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import RootLayout from './layout/RootLayout';
 import NotFound from './components/error/NotFound';
+import LoginPage from './features/auth/LoginPage';
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
       { path: '/plan', element: <p>일정 계획 페이지 컴포넌트</p> },
       { path: '/diary', element: <p>지난 여행 페이지 컴포넌트</p> },
       { path: '/place', element: <p>여행지 페이지 컴포넌트</p> },
+      { path: '/login', element: <LoginPage /> },
     ],
   },
 ]);
