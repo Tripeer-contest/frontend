517af6183057bc2f66a4bab89985caf87a4f2458
부수환 <rndjdieo119@naver.com>
2024-08-26 13:06:27 +0900

    fix : 안쓰는 변수 삭제

--- Tripeer/src/features/plan/layout/CardsLayout.tsx
+++ Tripeer/src/features/plan/layout/CardsLayout.tsx
@@ -2,23 +2,6 @@ import { Swiper } from 'swiper/react';
 import { ReactNode } from 'react';
 import 'swiper/css';
 
-import styles from '../assets/page.module.css';
-
-const breakpoints = {
-  320: {
-    slidesPerView: 1,
-    spaceBetween: 10,
-  },
-  730: {
-    slidesPerView: 2,
-    spaceBetween: 30,
-  },
-  1300: {
-    slidesPerView: 3,
-    spaceBetween: 100,
-  },
-};
-
 export default function CardsLayout({ children }: { children: ReactNode }) {
   return (
     <Swiper slidesPerView={'auto'} style={{ margin: 0 }} spaceBetween={20}>
