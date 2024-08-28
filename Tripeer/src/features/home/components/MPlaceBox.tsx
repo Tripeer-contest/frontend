b3368c7acc6884711cf9235dd55d43b0bfdeb05f
lhgeer7485 <lhgeer2617@gmail.com>
2024-08-28 17:22:04 +0900

    feat: 홈 반응형

--- Tripeer/src/features/home/components/MPlaceBox.tsx
+++ Tripeer/src/features/home/components/MPlaceBox.tsx
@@ -1,19 +1,33 @@
 import styles from '../modules/mPlaceBox.module.css';
 import testImg from '../assets/testImg.png';
+import like from '../assets/like.png';
+import unLike from '../assets/unLike.png';
 import { getRateImg } from '../../../utils/rating.ts';
+import usePlaceBox from '../hooks/usePlaceBox.tsx';
 
 const MPlaceBox = () => {
+  const { likeClickHandler, isLike, rating } = usePlaceBox();
+  const star = getRateImg(rating);
+
   return (
     <div className={styles.container}>
-      <img src={testImg} alt={'placeImg'} className={styles.image} />
+      <div className={styles.imageBox}>
+        <img src={testImg} alt={'placeImg'} className={styles.image} />
+        <img
+          src={isLike ? like : unLike}
+          alt={'likeImg'}
+          className={styles.like}
+          onClick={likeClickHandler}
+        />
+      </div>
       <section className={styles.description}>
         <p className={styles.title}>신라스테이 해운대</p>
         <div className={styles.ratingBox}>
-          <img src={getRateImg(4.5)} alt={'ratingImage'} />
-          <p>(4.5)</p>
+          <img src={star} alt={'ratingImage'} className={styles.ratingImg} />
+          <p className={styles.ratingP}>(4.5)</p>
         </div>
-        <p>카테고리 - 숙박</p>
-        <p>부산 해운대구 해운대로 570번길 46</p>
+        <p className={styles.desP}>카테고리 - 숙박</p>
+        <p className={styles.desP}>부산 해운대구 해운대로 570번길 46</p>
       </section>
     </div>
   );
