20486da371d17d73ba8124dc2786913ae903de03
hyunjihye <hhh259@naver.com>
2024-09-04 20:41:54 +0900

    feat : 플렌 디테일 맵화면 레이아웃 반응형 설계

--- Tripeer/src/features/planDetail/components/map/PlanMap.tsx
+++ Tripeer/src/features/planDetail/components/map/PlanMap.tsx
@@ -1,12 +1,112 @@
+import { useState } from 'react';
 import styles from '../../assets/map/map.module.css';
 import PlanShortNav from '../PlanShortNav';
+import MapLayout from './layout/MapLayout';
+import MapHeader from './MapHeader';
+import PlaceMap from './mapNav/PlaceMap';
+import SearchPlace from './mapNav/SearchPlace';
+import PlaceList from './mapNav/PlaceList';
 
 export default function PlanMap() {
+  const [isVisible, setIsVisible] = useState(false);
+  const [active, setActive] = useState('search');
+
+  const activeComponent = (value: string) => {
+    if (value === 'search' && isVisible === true) {
+      setIsVisible(false);
+    }
+    setActive(value);
+  };
+
+  const openPlaceList = () => {
+    if (active === 'list') {
+      setActive('search');
+    }
+    setIsVisible(!isVisible);
+  };
+
   return (
     <main className={styles.container}>
       <aside className={styles.nav}>
         <PlanShortNav />
       </aside>
+      <section className={styles.contentBox}>
+        <MapHeader />
+        <MapLayout>
+          {active === 'search' && (
+            <>
+              <div className={styles.noMobile}>
+                <SearchPlace
+                  isVisible={isVisible}
+                  openPlaceList={openPlaceList}
+                />
+                <PlaceMap />
+              </div>
+              <div className={styles.mobile}>
+                <SearchPlace
+                  isVisible={isVisible}
+                  openPlaceList={openPlaceList}
+                />
+              </div>
+            </>
+          )}
+          {active === 'list' && (
+            <>
+              <div className={styles.noMobile}>
+                <SearchPlace
+                  isVisible={isVisible}
+                  openPlaceList={openPlaceList}
+                />
+                <PlaceList />
+                <PlaceMap />
+              </div>
+              <div className={styles.mobile}>
+                <PlaceList />
+              </div>
+            </>
+          )}
+          {active === 'map' && (
+            <>
+              <div className={styles.noMobile}>
+                <SearchPlace
+                  isVisible={isVisible}
+                  openPlaceList={openPlaceList}
+                />
+                <PlaceMap />
+              </div>
+              <div className={styles.mobile}>
+                <PlaceMap />
+              </div>
+            </>
+          )}
+        </MapLayout>
+        <div className={styles.mapNavBox}>
+          <div
+            className={styles.mapBtnBox}
+            onClick={() => {
+              activeComponent('search');
+            }}
+          >
+            여행지 검색
+          </div>
+          <div
+            className={styles.mapBtnBox}
+            onClick={() => {
+              activeComponent('map');
+            }}
+          >
+            여행 지도
+          </div>
+          <div
+            className={styles.mapBtnBox}
+            onClick={() => {
+              activeComponent('list');
+            }}
+          >
+            여행지 목록
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
---
+++ Tripeer/src/features/planDetail/components/map/mapNav/PlaceList.tsx
@@ -0,0 +1,5 @@
+import styles from '../../../assets/map/mapNav/placeList.module.css';
+
+export default function PlaceList() {
+  return <div className={styles.container}>이거 여행지 목록</div>;
+}
