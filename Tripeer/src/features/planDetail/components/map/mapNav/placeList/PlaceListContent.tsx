1989d744259aadd0565f95fae18363488235254c
hyunjihye <hhh259@naver.com>
2024-09-13 00:52:38 +0900

    feat: 여행지 목록 카드 레이아웃 제작, town선택 시 map 이동

--- Tripeer/src/features/planDetail/components/map/mapNav/placeList/PlaceListContent.tsx
+++ Tripeer/src/features/planDetail/components/map/mapNav/placeList/PlaceListContent.tsx
@@ -1,8 +1,9 @@
 import styles from '../../../../assets/map/mapNav/placeListContent.module.css';
+import PlaceListCard from './PlaceListCard';
 export default function PlaceContent() {
   return (
     <div className={styles.container}>
-      <p>dd</p>
+      <PlaceListCard />
     </div>
   );
 }
