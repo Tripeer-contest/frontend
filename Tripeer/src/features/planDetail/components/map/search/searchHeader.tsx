139e06819f634b25a5bf635e1881c2968fca20eb
hyunjihye <hhh259@naver.com>
2024-09-18 18:49:03 +0900

    fix : 스크롤 스타일 변경

--- Tripeer/src/features/planDetail/components/map/search/searchHeader.tsx
+++ Tripeer/src/features/planDetail/components/map/search/searchHeader.tsx
@@ -1,8 +1,10 @@
+import useModal from '../../../../../hooks/useModal';
 import styles from '../../../assets/map/header.module.css';
 import PlanHamburger from '../../common/PlanHamburger';
 import TownController from '../mapNav/TownController';
 
 export default function SearchHeader() {
+  const { ModalLayout, close, open } = useModal();
   return (
     <>
       <header className={styles.header}>
@@ -11,9 +13,14 @@ export default function SearchHeader() {
             <PlanHamburger />
             <TownController />
           </div>
-          <div className={styles.addPlaceBtn}>신규 장소 등록</div>
+          <div className={styles.addPlaceBtn} onClick={open}>
+            신규 장소 등록
+          </div>
         </div>
       </header>
+      <ModalLayout>
+        <button onClick={close}>close</button>
+      </ModalLayout>
     </>
   );
 }
