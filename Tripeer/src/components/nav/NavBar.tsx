008579ae66c8e2da4b16347aa3e6bd5b353dc9af
부수환 <rndjdieo119@naver.com>
2024-09-18 19:03:49 +0900

    style: home css 수정

--- Tripeer/src/components/nav/NavBar.tsx
+++ Tripeer/src/components/nav/NavBar.tsx
@@ -1,4 +1,5 @@
 // 내부 모듈
+import SearchBar from '../../features/home/components/SearchBar';
 import FullLogo from './FullLogo';
 import styles from './navbar.module.css';
 import NavButton from './NavButton';
@@ -7,6 +8,7 @@ export default function NavBar() {
   return (
     <nav className={styles.container}>
       <FullLogo />
+      <SearchBar />
       <ul className={styles.linkBox}>
         <NavButton to="/home" text="홈" />
         <NavButton to="/plan" text="일정 계획" />
