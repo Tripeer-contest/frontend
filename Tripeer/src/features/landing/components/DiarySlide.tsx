2f6d34c66c15fa2212126360b1f896387d7e924d
hyunjihye <hhh259@naver.com>
2024-09-21 18:59:17 +0900

    fix: 랜딩페이지 스크롤 수정 및 이미지 전면 수정

--- Tripeer/src/features/landing/components/DiarySlide.tsx
+++ Tripeer/src/features/landing/components/DiarySlide.tsx
@@ -1,23 +1,25 @@
-import useAnimation from "../hook/useAnimation"
-import { useRef } from "react"
-import styles from "./diarySlide.module.css"
+import useAnimation from '../hook/useAnimation';
+import { useRef } from 'react';
+import styles from './diarySlide.module.css';
+import banner from '../assets/landingDiary.png';
 
-export default function DiarySlide(): JSX.Element{
-    const titleRef = useRef<null | HTMLHeadingElement>(null)
-    const bannerRef = useRef<null | HTMLDivElement>(null)
+export default function DiarySlide(): JSX.Element {
+  const titleRef = useRef<null | HTMLHeadingElement>(null);
+  const bannerRef = useRef<null | HTMLDivElement>(null);
+  useAnimation([titleRef, bannerRef]);
 
-    useAnimation([titleRef,bannerRef])
-
-    return(
-        <main className={styles.container}>
-            <section className={styles.box1}>
-                <article className={styles.titleBox}>
-                    <h1 className={styles.title} ref={titleRef}>여행의 모든 순간을 상세히 기록하고, <br/>사진과 함께 저장하세요.</h1>
-                </article>
-            </section>
-            <figure className={styles.box2}>
-                <div className={styles.banner} ref={bannerRef}></div>
-            </figure>
-        </main>
-    )
+  return (
+    <main className={styles.container}>
+      <section className={styles.box1}>
+        <h1 className={styles.title} ref={titleRef}>
+          여행의 모든 순간을,
+          <br />
+          함께 기록하고 추억하세요.
+        </h1>
+      </section>
+      <figure className={styles.box2} ref={bannerRef}>
+        <img src={banner} alt="diary-img" className={styles.banner} />
+      </figure>
+    </main>
+  );
 }
