489a5035e033f625244d284b4eeef470c43bd492
부수환 <rndjdieo119@naver.com>
2024-09-28 18:21:09 +0900

    test : web to flutter

--- Tripeer/src/utils/sendFlutter.ts
+++ Tripeer/src/utils/sendFlutter.ts
@@ -7,12 +7,15 @@ declare global {
   }
 }
 
-export function sendFlutterToSendToken(setter?: (param: boolean) => void) {
+export async function sendFlutterToSendToken(setter?: (param: any) => void) {
   if (window.flutter_inappwebview) {
-    setter && setter(true);
-    window.flutter_inappwebview.callHandler('webToFlutterFCM', {
-      message: 'sendToken',
-      data: { token: 'test' },
-    });
+    const res = await window.flutter_inappwebview.callHandler(
+      'webToFlutterFCM',
+      { message: 'getToken' },
+    );
+    setter && setter(res);
+    return res;
+  } else {
+    return null;
   }
 }
