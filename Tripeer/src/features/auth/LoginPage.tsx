16fe6368cb15c05c3ff33c65b99486f2e04ed944
부수환 <rndjdieo119@naver.com>
2024-10-01 15:00:54 +0900

    feat : 소셜로그인 plus alpha

--- Tripeer/src/features/auth/LoginPage.tsx
+++ Tripeer/src/features/auth/LoginPage.tsx
@@ -4,25 +4,43 @@ import styles from './login.module.css';
 import { Link } from 'react-router-dom';
 import SocialButton from './components/SocialButton';
 import src from './assets/bottom.svg';
+import { Fragment, useEffect, useState } from 'react';
+import getDeviceInfo from '../../utils/sendFlutter';
 
 export default function LoginPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [socialArr, setSocialArr] = useState<string[]>([]);
+  useEffect(() => {
+    const getSocial = async () => {
+      const social = await getDeviceInfo();
+      setSocialArr(social);
+      setIsLoading(false);
+    };
+    getSocial();
+  }, []);
   return (
     <LoginLayout>
       <Link to="/">
         <img src={Logo} alt="tripeer-logo" className={styles.Logo} />
       </Link>
-      <div className={styles.borderBox}>
-        <div className={styles.line} />
-        <p>로그인&회원가입</p>
-        <div className={styles.line} />
-      </div>
-      <SocialButton social="google" />
-      <SocialButton social="kakao" />
-      <SocialButton social="naver" />
-      <div className={styles.center}>
-        <img src={src} alt={'bottom'} />
-        <p>About Tripeer</p>
-      </div>
+      {isLoading ? undefined : (
+        <>
+          <div className={styles.borderBox}>
+            <div className={styles.line} />
+            <p>로그인&회원가입</p>
+            <div className={styles.line} />
+          </div>
+          {socialArr.map((social, idx) => (
+            <Fragment key={idx}>
+              <SocialButton social={social} />
+            </Fragment>
+          ))}
+          <div className={styles.center}>
+            <img src={src} alt={'bottom'} />
+            <p>About Tripeer</p>
+          </div>
+        </>
+      )}
     </LoginLayout>
   );
 }
--- Tripeer/src/utils/sendFlutter.ts
+++ Tripeer/src/utils/sendFlutter.ts
@@ -30,3 +30,24 @@ export async function setFlutterToSendPermission() {
     return null;
   }
 }
+
+export async function sendFlutterToSendDeviceInfo() {
+  if (window.flutter_inappwebview) {
+    const res =
+      await window.flutter_inappwebview.callHandler('webToFlutterDevice');
+    return res;
+  } else {
+    return 'BROWSER';
+  }
+}
+
+export default async function getDeviceInfo() {
+  try {
+    const res = await sendFlutterToSendDeviceInfo();
+    if (res === 'ANDROID' || res === 'BROWSER')
+      return ['google', 'kakao', 'naver'];
+    else return ['apple', 'kakao', 'naver'];
+  } catch {
+    return ['google', 'kakao', 'naver'];
+  }
+}
