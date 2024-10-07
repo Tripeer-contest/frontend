0c8fbdbf061119989b30dd3d3f1bdc0ab6249ed6
lhgeer7485 <lhgeer2617@gmail.com>
2024-10-07 16:18:35 +0900

    fix : 회원정보 등록 ui 오류 수정

--- Tripeer/src/features/register/components/StyleChip.tsx
+++ Tripeer/src/features/register/components/StyleChip.tsx
@@ -1,7 +1,6 @@
 import styles from './styleChip.module.css';
 import React from 'react';
 import useStyleChip from '../hooks/useStyleChip.tsx';
-import zustandStore from '../../../store/store.tsx';
 
 interface Props {
   idx: number;
@@ -10,12 +9,12 @@ interface Props {
 }
 
 const StyleChip: React.FC<Props> = ({ idx, title, image }) => {
-  const { onClickHandler } = useStyleChip();
-  const r_style = zustandStore((state) => state.r_style);
+  const { onClickHandler, r_style } = useStyleChip();
+  const isCheck = r_style.includes(idx + 1);
 
   return (
     <main
-      className={`${styles.body} ${r_style.includes(idx + 1) ? styles.check : ''}`}
+      className={`${styles.body} ${isCheck ? styles.check : ''}`}
       onClick={() => onClickHandler(idx)}
     >
       <img src={image} alt={'styleChip'} className={styles.image} />
--- Tripeer/src/features/register/hooks/useStyleChip.tsx
+++ Tripeer/src/features/register/hooks/useStyleChip.tsx
@@ -1,5 +1,6 @@
 import zustandStore from '../../../store/store.tsx';
 import { useShallow } from 'zustand/react/shallow';
+import { useEffect } from 'react';
 
 const useStyleChip = () => {
   const [r_style, r_setStyle] = zustandStore(
@@ -12,11 +13,15 @@ const useStyleChip = () => {
     } else {
       if (r_style.length < 3) {
         r_setStyle([...r_style, idx + 1]);
+      } else {
+        console.log(r_style);
       }
     }
   };
 
-  return { onClickHandler };
+  useEffect(() => {}, [r_style]);
+
+  return { onClickHandler, r_style };
 };
 
 export default useStyleChip;
